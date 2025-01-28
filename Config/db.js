ddb1910f5a6573f22485e4b7324ce0d1f6525fa4
Gili <g8566650@gmail.com>
2025-01-28 16:58:35 +0200

    Updated server connection and fixed port listening issue

--- Config/db.js
+++ Config/db.js
@@ -2,7 +2,7 @@ import {connect}from "mongoose";
 
 export const connectToDb=async(req,res)=>{
     try{
-        let con=await connect(process.env.DB_URL)
+        let con=await connect(process.env.DB_URL||5500)
         console.log("mongo db connected")
     }
     catch(err){
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv'; 
-import cors from  "cors";
+import cors from "cors";
 
-
-import userRouter from "./Routers/user.js"
-import productRouter from "./Routers/product.js"
-import orderRouter from "./Routers/order.js"
+import userRouter from "./Routers/user.js";
+import productRouter from "./Routers/product.js";
+import orderRouter from "./Routers/order.js";
 import { connectToDb } from './Config/db.js';
-
 
 dotenv.config();
 // חיבור למסד הנתונים
 connectToDb();
+
 const app = express();
+
+// הגדרת CORS
 app.use(cors({ origin: "http://localhost:5500" }));
-app.use(express.json())
- 
+app.use(express.json());
 
-app.use("/api/user",userRouter);
-app.use("/api/product",productRouter);
-app.use("/api/order",orderRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
 
-const port = process.env.PORT 
-// הפעלת השרת
-app.listen(port,"localhost",()=>{
-    console.log("app is listening on port"+port)
-})
+// הגדרת הפורט
+const port = process.env.PORT || 5500;
 
-
+// הפעלת השרת עם כתובת '0.0.0.0' כדי ש־Render יוכל לגשת
+app.listen(port, "0.0.0.0", () => {
+    console.log(`App is listening on port ${port}`);
+});
